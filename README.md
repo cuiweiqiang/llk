36029696b39b7afa4250f5bcee46f3079fa9613b
cuiweiqiang <cuiweiqiang.dlnu@gmail.com>
2018-06-21 18:32:36 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+LLK
+=======
+
+个人仿照[egg-init](https://github.com/eggjs/egg-init) 做的一个koa脚手架
+
+## Install
+
+```bash
+$ npm i llk -g
+$ llk -h
+```
+
+## License
+
+[MIT](LICENSE)
---
+++ bin/llk.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: cuiweiqiang
+ * @Date:   2018-06-21 16:55:55
+ * @Last Modified by:   cuiweiqiang
+ * @Last Modified time: 2018-06-21 17:19:40
+ */
+
+'use strict';
+
+const co = require('co');
+const Command = require('..');
+
+co(function*() {
+    yield new Command().run(process.cwd(), process.argv.slice(2));
+}).catch(err => {
+    console.error(err.stack);
+    process.exit(1);
+});
